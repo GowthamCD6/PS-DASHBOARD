64f4ae8a553edd6b64c3c6427e468681074a457a
rithishcodespace <rithishcodespace@gmail.com>
2025-08-20 08:59:47 +0530

    added requirement

--- backend/routes/getRoute.js
+++ backend/routes/getRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const getController = require("../controllers/getController")
 
-router.get("/student/user_data/:id",getController.user_data);
+router.get("/student/user_data/:id",getController.user_data); // need year and dept
 
 module.exports = router;
