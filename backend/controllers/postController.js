2618ab258405bdb397171312b3f8fc0237aa9ed1
rithishcodespace <rithishcodespace@gmail.com>
2025-08-27 09:37:15 +0530

    added remove mentor

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -6,27 +6,53 @@ exports.assign_mentor = (req,res,next) => {
       const{faculty_id, student_id, status} = req.params;
       if(!faculty_id.trim() || !student_id.trim() || !status.trim())return next(createError.BadRequest("id is invalid!"));
       let allowedStatus = ['1','3'];
-      if(!allowedStatus.contains(status))return createError.BadRequest('Invalid status!');
+      if(!allowedStatus.includes(status))return createError.BadRequest('Invalid status!');
       // check whether the id is a teacher's id
       let isfaculty = 'select type from master_user where id = ?';
       db.query(isfaculty,[faculty_id],(error,result) => {
-        if(error || result.trim() == ""){
+        if(error || result.length() == 0){
             return next(error || createError.BadRequest('id type not foud'))
         }
-        if(result.type != 'teacher')return next("Invalid id");
+        if(result[0].type != 'teacher')return next("Invalid id");
         // check if the faculty is already a mentor of that student
         let checksql = "select relationship from master_relationship_mapping where user = ? and relation_user = ?";
         db.query(checksql,[faculty_id, student_id],(error1, result1) => {
             if(error1 || result1.length > 0){
                 return next(error1 || createError.BadRequest("The faculty is already to the specified student!"));
             }
+            if(status == '1'){
+                // allowing only one permanent mentor
+                let cntpermsql = "select * from master_relationship_mapping where relation_user = ? and relationship = '1'";
+                db.query(cntpermsql,[student_id],(error2,result2) => {
+                    if(error2 || result2.length > 0){
+                        return next(error2 || createError.BadRequest("Permanent mentor already exist!"));
+                    }
+                })
+
+            }
             // assign the faculty as the temp || permanent based on the status
             let assignsql = "insert into master_relationship_mapping (relationship, user, relation_user, status) values(?, ?, ?, ?);"
-            db.query(assignsql,[status, faculty_id, student_id, status], (error2,result2) => {
-                if(error2)return next(error2);
+            db.query(assignsql,[status, faculty_id, student_id, status], (error3,result3) => {
+            if(error3)return next(error3);
                 return res.send('faculty assgined successfully!');
             })
+            
         })
+      })
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+exports.remove_mentor = (req,res,next) => {
+    try{
+      const{faculty_id, student_id} = req.params;
+      if(!faculty_id || !student_id.trim())return next(createError.BadRequest("required paramenters are missing!"));
+      let remove_mentor = "delete from master_relationship_mapping where user = ? and relation_user = ?";
+      db.query(remove_mentor,[faculty_id, student_id],(error,result) => {
+        if(error || result.affectedRows == 0)return next(error || createError.BadRequest("No mentor found to delete!"));
+        return res.send("Mentor deleted successfully!");
       })
     }
     catch(error){
--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
 
-router.post("/faculty/assign_mentor/:faculty_id/:student_id/:status",postController.assign_mentor);
+router.post("/faculty/assign_mentor/:faculty_id/:student_id/:status",postController.assign_mentor); // handles both perm and temp
+router.delete("/faculty/remove_mentor/:facutly_id/:student_id/:status",postController.remove_mentor); // removes both permanent and temp
 
 module.exports = router;
