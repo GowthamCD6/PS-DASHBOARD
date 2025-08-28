094714448536dc13e2e2fbc757f1c546a2019427
rithishcodespace <rithishcodespace@gmail.com>
2025-08-28 13:56:50 +0530

    removed error

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const postController = require("../controllers/postController");
 
 router.post("/faculty/assign_mentor/:faculty_id/:student_id/:role_id/:status",postController.assign_mentor); // handles both perm and temp
-router.delete("/faculty/remove_mentor/:facutly_id/:student_id/:status",postController.remove_mentor); // removes both permanent and temp
+router.delete("/faculty/remove_mentor/:faculty_id/:student_id/:status",postController.remove_mentor); // removes both permanent and temp
 
 module.exports = router;
