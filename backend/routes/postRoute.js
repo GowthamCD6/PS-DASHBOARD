122b9fca543039d471f0c343a2e48e700edb30fa
rithishcodespace <rithishcodespace@gmail.com>
2025-09-20 16:00:35 +0530

    Fix: updated postRoute

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -6,4 +6,5 @@ const userAuth = require("../middlewares/userAuth")
 router.post("/faculty/assign_mentor/:faculty_id/:student_id/:role_id/:status",postController.assign_mentor); // handles both perm and temp
 router.delete("/faculty/remove_mentor/:faculty_id/:student_id/:status",postController.remove_mentor); // removes both permanent and temp
 
+
 module.exports = router;
